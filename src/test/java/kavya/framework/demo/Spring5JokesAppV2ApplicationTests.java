b50e43d87337563958a4210ddade536b403c56c2
Kavya Singh <ksingh@manh.com>
2021-03-28 11:24:22 +0530

    Initial checkin closes #1

---
+++ src/test/java/kavya/framework/demo/Spring5JokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kavya.framework.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
